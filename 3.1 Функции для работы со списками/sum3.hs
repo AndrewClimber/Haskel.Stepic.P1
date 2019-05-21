0eb99a41ab7d77553d8e22b684a11fc8463a6013
AndrewClimber <seoclimber@gmail.com>
2019-05-21 11:37:54 +0700

    sum3

--- 3.1 Функции для работы со списками/sum3.hs
+++ 3.1 Функции для работы со списками/sum3.hs
@@ -1,3 +1,4 @@
+import Data.List (transpose)
 {-
 Составьте список сумм соответствующих элементов трех заданных списков. Длина результирующего списка должна быть равна длине самого 
 длинного из заданных списков, при этом «закончившиеся» списки не должны давать вклада в суммы.
@@ -6,48 +7,39 @@ GHCi> sum3 [1,2,3] [4,5] [6]
 [11,7,3]
 -}
 
---oddsOnly'''' = \xs -> [x | x <- xs, odd x]
---oddsOnly'' (x:xs) = [x | x <- x:xs, odd x]
---oddsOnly (x:xs) =  if even x then ys else x : ys 
-          --where ys = if null xs then [] else oddsOnly xs
-
-
-
---sum3  =  \as bs cs -> [x+y+z | x <- as, y <- bs, z <- cs, not(null as), not (null bs), not (null cs)]
---sum3  =  \as bs cs -> [x+y+z | x <- as, y <- bs, z <- cs]
---sum3  (a:as) (b:bs) (c:cs) =  [x+y+z | x <- a:as, y <- b:bs, z <- c:cs]
---sum3 [] [] [] = []
-
-{-
-sum3  (a:as) (b:bs) (c:cs) | null as = sum3  [0] (b:bs) (c:cs)
-                           | null bs = sum3  (a:as) [0] (c:cs)
-                           | null cs = sum3  (a:as) (b:bs) [0] 
-                           | otherwise = a+b+c : sum3  as bs cs
-                           -}
-
---oddsOnly (x:xs) =  if even x then ys else x : ys 
-    --where ys = if null xs then [] else oddsOnly xs
-
-
-
 
 
 sum3 :: Num a => [a] -> [a] -> [a] -> [a]
 {-
-sum3  (a:as) (b:bs) (c:cs) = (isNulled a)+(isNulled b)+(isNulled c) : sum3  as bs cs where
-    isNulled n = if null n then 0 else n
-    -}
-
---sum3 [] [] [] = []
-sum3  (a:as) (b:bs) (c:cs) = if null as && null bs && null cs  then [] else a + b + c : sum3  (isNull as) (isNull bs) (isNull cs) where
+sum3  (a:as) (b:bs) (c:cs) = if null as && null bs && null cs  then [] else a + b + c : sum3 (isNull as) (isNull bs) (isNull cs) where
     isNull ns = if null ns then [0] else ns
-
-    
-
-
-
 sum3 _ _ _ = []
---sum3 [] [] [] = []
---sum3  (a:as) (b:bs) (c:cs) = a + b + c : sum3  as bs cs
+-}
+-- мое решение
+sum3 = \as bs cs -> [sum x | x <- transpose $ [as,bs,cs]]
 
---sum3  _ _ _  = _ + _ + _ : _ _ _
+-- stepik
+sum3' :: Num a => [a] -> [a] -> [a] -> [a]
+sum3' a b c = map sum (transpose [a,b,c])
+
+sum3'' a b c = ((map sum) . transpose) [a,b,c]
+
+{-
+sum3 :: Num a => [a] -> [a] -> [a] -> [a]
+sum3 [] [] [] = []
+sum3 [] ys zs = sum3 [0] ys zs
+sum3 xs [] zs = sum3 xs [0] zs
+sum3 xs ys [] = sum3 xs ys [0]
+sum3 (x:xs) (y:ys) (z:zs) = x+y+z : sum3 xs ys zs
+
+-- через sum2
+sum3 :: Num a => [a] -> [a] -> [a] -> [a]
+sum3 xs ys zs = xs `sum2` ys `sum2` zs
+  where
+    sum2 [] bs = bs
+    sum2 as [] = as
+    sum2 (a : as) (b : bs) = (a + b) : sum2 as bs
+
+
+
+    -}
