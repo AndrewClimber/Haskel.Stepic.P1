2cd1b74e58e78fb790679c77e28a98b73d815307
AndrewClimber <seoclimber@gmail.com>
2019-06-18 18:03:30 +0700

    18.06.2019

--- 3.3 Генераторы списков/fibStream.hs
+++ 3.3 Генераторы списков/fibStream.hs
@@ -10,8 +10,15 @@ fibStream = fib 0 1 where
     fib n1 n2 | (n1 == 0 && n2 ==1) = [0,1,1] ++ fib (2) (3)
               | otherwise =  (zipWith (+) [n1] [n2]) ++ fib (n1+1) (n2+1)
 -}
-fibStream :: [Integer]
-fibStream = fs 0 where 
+
+--fibStream :: [Integer]
+fibStream n = 0:1:zipWith (+) [(fibStream !! n)] [(fibStream !! (n+1))]
+
+
+
+
+fibStream' :: [Integer]
+fibStream' = fs 0 where 
     fs n = fibonacci (n) : fs (n+1) where 
         fibonacci 0 = 0
         fibonacci 1 = 1
