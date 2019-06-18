d9e5677b6ddf6ac2da3d442a940ed9d66cd75fae
AndrewClimber <seoclimber@gmail.com>
2019-06-18 12:19:15 +0700

    18.06.2019

--- 3.2 Функции высших порядков над списками/m3.hs
+++ 3.2 Функции высших порядков над списками/m3.hs
@@ -8,6 +8,10 @@ GHCi> max3 [7,2,9] [3,6,8] [1,8,10]
 GHCi> max3 [7,2,9,11] [3,6,8,12] [1,8,10,13]
 [7,8,10,13]
 -}
+
+import Data.List
+
+-- мое решение. Когда плохо знаешь Prelude или Data.List
 max3 :: Ord a => [a] -> [a] -> [a] -> [a]
 max3 a1 a2 a3 = maxi3 a1 a2 a3 where
     maxi3 a1 a2 a3 = map maximum  (concat3 0 (length a1)) where
@@ -16,4 +20,10 @@ max3 a1 a2 a3 = maxi3 a1 a2 a3 where
         concat3 n l | n < l = ([(elpos3 n l (n+l+l))] ++ (concat3 (n+1) l))
                     | otherwise = []
         
-        
+-- stepik
+max3' a1 a2 a3 = map maximum $ transpose [a1, a2, a3]
+
+max3'' x y z = map (\(x',y',z') -> max z' $ max x' y') $ zip3 x y z
+
+-- как это работает не понял.
+max3''' = zipWith3 ((max .) . max)
