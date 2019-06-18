4c16973cb13310c7a6c91a4f86e6cded9d4e41d4
AndrewClimber <seoclimber@gmail.com>
2019-06-18 09:17:55 +0700

    18.06.2019

--- 3.2 Функции высших порядков над списками/m3.hs
+++ 3.2 Функции высших порядков над списками/m3.hs
@@ -8,12 +8,12 @@ GHCi> max3 [7,2,9] [3,6,8] [1,8,10]
 GHCi> max3 [7,2,9,11] [3,6,8,12] [1,8,10,13]
 [7,8,10,13]
 -}
---max3 :: Ord a => [a] -> [a] -> [a] -> [a]
-max3 a1 a2 a3 = m3 a1 a2 a3 where
-    m3 a1 a2 a3 = map maximum  (con3 0 (length a1)) where
+max3 :: Ord a => [a] -> [a] -> [a] -> [a]
+max3 a1 a2 a3 = maxi3 a1 a2 a3 where
+    maxi3 a1 a2 a3 = map maximum  (concat3 0 (length a1)) where
         elpos3 n1 n2 n3 = [glu3 !! n1 , glu3 !! n2 , glu3 !! n3]
         glu3 = a1 ++ a2 ++ a3
-        --concat3 l = [(elpos3 0 l (l+l))] ++ [(elpos3 1 (1+l) (1+l+l))] ++  [(elpos3 2 (2+l) (2+l+l))] ++  [(elpos3 3 (3+l) (3+l+l))]
-        con3 n l | n < l-1 = ([(elpos3 n l (n+l+l))] ++ (con3 n+1 l))
+        concat3 n l | n < l = ([(elpos3 n l (n+l+l))] ++ (concat3 (n+1) l))
+                    | otherwise = []
         
         
